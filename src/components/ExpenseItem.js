7803e9f7e3b0085bf82aeae836968f5f2922e39e
rajk5266 <kushwaharaj903@gmail.com>
2023-09-27 00:25:45 +0530

    Expense Item added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
- function ExpenseItem(){
-    return <h2>
-        Expennse Items!
-    </h2>
- }
+function ExpenseItem() {
+  return (
+    <div>
+      <div>01/08/2023</div>
+      <div>
+        <h2>Fuel</h2>
+        <div>500</div>
+      </div>
+    </div>
+  );
+}
 
- export default ExpenseItem;
+export default ExpenseItem;
