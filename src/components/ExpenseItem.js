24b954767c4cc66c3890440ef341f130483653c6
rajk5266 <kushwaharaj903@gmail.com>
2023-09-27 01:06:41 +0530

    location of expenditure added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,28 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+  const expenseDate = new Date();
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+  const expenseLocation = 'McD'
+
   return (
-    <div>
-      <div>01/08/2023</div>
-      <div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString().slice(0,10)}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h3>{expenseLocation}</h3>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+      <div>03/08/2023</div>
+      <div className="expense-item__description">
         <h2>Fuel</h2>
-        <div>500</div>
+        <div className="expense-item__price">500</div>
+      </div>
+      <div>08/08/2023</div>
+      <div className="expense-item__description">
+        <h2>Movies</h2>
+        <div className="expense-item__price">200</div>
       </div>
     </div>
   );
