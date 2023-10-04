0a4ee0abf4fe6a315bd97936c380477f68b962d7
rajk5266 <kushwaharaj903@gmail.com>
2023-10-04 20:18:08 +0530

    adding Event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,24 @@ import ExpenseDate from  './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card  from '../UI/Card';
 import './ExpenseItem.css'
-
+import React, { useState} from 'react'
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+      setTitle('new Title');
+      
+  }
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-      <ExpenseDetails title={props.title} amount={props.amount}/>
+      <ExpenseDetails title={title} amount={props.amount}/>
         <h3>{props.location}</h3>
       </div>
+      <button onClick={clickHandler} >Edit</button>
+      <button  >Delete</button>
     </Card>
   );
 }
